6bd9769509641cc8678229ece24a9ec98bb42df4
tanakaworld <yutaro.tanaka.world@gmail.com>
2020-08-24 15:34:08 +0900

    test: add a valid case

---
+++ cypress/integration/describe-it-with-as.spec.ts
@@ -0,0 +1,39 @@
+describe("describe-it-with-as", () => {
+  before(() => {
+    cy.request("https://reqres.in/api/users/1").then((response) => {
+      cy.wrap(response).as("response");
+    });
+  });
+
+  describe("describe1", () => {
+    it("-it1", function () {
+      console.log("describe1-it1", this.response);
+      cy.visit("https://example.cypress.io/").then(function () {
+        console.log("describe1-it1 after visit", this.response);
+      });
+    });
+
+    it("-it2", function () {
+      console.log("describe1-it2", this.response);
+      cy.visit("https://example.cypress.io/").then(function () {
+        console.log("describe1-it2 after visit", this.response);
+      });
+    });
+  });
+
+  describe("describe2", () => {
+    it("-it1", function () {
+      console.log("describe2-it1", this.response);
+      cy.visit("https://example.cypress.io/").then(function () {
+        console.log("describe2-it1 after visit", this.response);
+      });
+    });
+
+    it("-it2", function () {
+      console.log("describe2-it2", this.response);
+      cy.visit("https://example.cypress.io/").then(function () {
+        console.log("describe2-it2 after visit", this.response);
+      });
+    });
+  });
+});
