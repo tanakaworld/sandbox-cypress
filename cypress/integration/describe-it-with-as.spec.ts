b8cbf570b36c940013a9ce8334d0d4b051f27285
tanakaworld <yutaro.tanaka.world@gmail.com>
2020-08-24 18:37:17 +0900

    test(verify): use session storage

--- cypress/integration/describe-it-with-as.spec.ts
+++ cypress/integration/describe-it-with-as.spec.ts
@@ -1,46 +1,73 @@
 describe("describe-it-with-as", () => {
   before(() => {
-    cy.request("https://reqres.in/api/users/1").then((response) => {
+    cy.request("https://reqres.in/api/users/1").then((response: any) => {
       cy.wrap(response).as("response");
+
+      cy.saveJSONAs("response", response);
     });
   });
 
   describe("describe1", () => {
     it("-it1", function () {
-      console.log("describe1-it1", this.response);
+      cy.getJSON("response").then((json) => {
+        console.log("describe1-it1", json);
+      });
+
       cy.visit("https://example.cypress.io/").then(function () {
-        console.log("describe1-it1 after visit", this.response);
+        cy.getJSON("response").then((json) => {
+          console.log("describe1-it1 after visit", json);
+        });
       });
     });
 
     it("-it2", function () {
-      console.log("describe1-it2", this.response);
+      cy.getJSON("response").then((json) => {
+        console.log("describe1-it2", json);
+      });
+
       cy.visit("https://example.cypress.io/").then(function () {
-        console.log("describe1-it2 after visit", this.response);
+        cy.getJSON("response").then((json) => {
+          console.log("describe1-it2 after visit", json);
+        });
       });
     });
   });
 
   describe("describe2", () => {
     it("-it1", function () {
-      console.log("describe2-it1", this.response);
+      cy.getJSON("response").then((json) => {
+        console.log("describe2-it1", json);
+      });
+
       cy.visit("https://example.cypress.io/").then(function () {
-        console.log("describe2-it1 after visit", this.response);
+        cy.getJSON("response").then((json) => {
+          console.log("describe2-it1 after visit", json);
+        });
       });
     });
 
     it("-it2", function () {
-      console.log("describe2-it2", this.response);
+      cy.getJSON("response").then((json) => {
+        console.log("describe2-it2", json);
+      });
+
       cy.visit("https://example.cypress.io/").then(function () {
-        console.log("describe2-it2 after visit", this.response);
+        cy.getJSON("response").then((json) => {
+          console.log("describe2-it2 after visit", json);
+        });
       });
     });
   });
 
   it("top level it1", function () {
-    console.log("top level it1", this.response);
+    cy.getJSON("response").then((json) => {
+      console.log("top level it", json);
+    });
+
     cy.visit("https://example.cypress.io/").then(function () {
-      console.log("top level it1 after visit", this.response);
+      cy.getJSON("response").then((json) => {
+        console.log("top level it after visit", json);
+      });
     });
   });
 });
